e6edcd8c447fc2bf1db65d70057d368c0bdbe606
Tonnerre LOMBARD <tonnerre@ancient-solutions.com>
2014-07-09 15:46:32 +0200

    Add a caching key server client.
    Also add a test program for the API.

---
+++ client.go
@@ -0,0 +1,170 @@
+/*
+ * (c) 2014, Tonnerre Lombard <tonnerre@ancient-solutions.com>,
+ *	     Starship Factory. All rights reserved.
+ *
+ * Redistribution and use in source  and binary forms, with or without
+ * modification, are permitted  provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of  source code  must retain the  above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this  list of conditions and the  following disclaimer in
+ *   the  documentation  and/or  other  materials  provided  with  the
+ *   distribution.
+ * * Neither  the name  of the Starship Factory  nor the  name  of its
+ *   contributors may  be used to endorse or  promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS"  AND ANY EXPRESS  OR IMPLIED WARRANTIES  OF MERCHANTABILITY
+ * AND FITNESS  FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT
+ * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package x509keyserver
+
+import (
+	"code.google.com/p/goprotobuf/proto"
+	"crypto/x509"
+	"expvar"
+	"net/rpc"
+	"sync"
+	"time"
+)
+
+// Implementation of the X.509 key server RPC interface from the client side.
+// Essentially implements a caching client which will keep up to
+// "max_cache_size" records in its cache. They never expire since certificate
+// index numbers shouldn't be reused and should therefor be unique.
+type X509KeyClient struct {
+	client               *rpc.Client
+	key_cache            map[uint64]*cacheRecord
+	cache_lock           sync.RWMutex
+	max_cache_size       int
+	cache_prune_interval time.Duration
+}
+
+type cacheRecord struct {
+	Cert     *x509.Certificate
+	LastUsed time.Time
+}
+
+var key_cache_size = expvar.NewInt("x509-key-cache-size")
+var key_cache_requests = expvar.NewInt("x509-key-cache-requests")
+var key_cache_hits = expvar.NewInt("x509-key-cache-hits")
+var key_cache_misses = expvar.NewInt("x509-key-cache-misses")
+var key_cache_errors = expvar.NewMap("x509-key-cache-errors")
+
+// Create a new caching X509 key client. "server" will be the server to
+// connect to for retrieving certificates, "max_size" is the maximum size
+// we'll want the cache to have, and "cache_prune_interval" is the
+// maximum amount of time we'll allow the cache to go over quota.
+func NewX509KeyClient(
+	server string,
+	max_size int,
+	cache_prune_interval time.Duration) (*X509KeyClient, error) {
+	var ret *X509KeyClient
+	var client *rpc.Client
+	var err error
+
+	client, err = rpc.DialHTTP("tcp", server)
+	if err != nil {
+		return nil, err
+	}
+
+	ret = &X509KeyClient{
+		client:               client,
+		key_cache:            make(map[uint64]*cacheRecord),
+		max_cache_size:       max_size,
+		cache_prune_interval: cache_prune_interval,
+	}
+
+	// Spawn a thread to regularly trim the cache back to its maximum size.
+	go ret.TrimCache()
+	return ret, nil
+}
+
+// Clean up old certificate entries.
+func (cl *X509KeyClient) TrimCache() {
+	c := time.Tick(cl.cache_prune_interval)
+
+	// Negative cache sizes don't make any sense.
+	if cl.max_cache_size < 0 {
+		return
+	}
+
+	for _ = range c {
+		cl.cache_lock.Lock()
+		for len(cl.key_cache) > cl.max_cache_size {
+			var cur, min *cacheRecord
+			var key, min_key uint64
+			for key, cur = range cl.key_cache {
+				if min == nil || cur.LastUsed.Before(min.LastUsed) {
+					min_key = key
+					min = cur
+				}
+			}
+
+			// We found a minimal key, delete it.
+			if min != nil {
+				delete(cl.key_cache, min_key)
+				key_cache_size.Set(int64(len(cl.key_cache)))
+			}
+		}
+		cl.cache_lock.Unlock()
+	}
+}
+
+// Retrieve the certificate associated with the given key ID.
+func (cl *X509KeyClient) RetrieveCertificateByIndex(index uint64) (*x509.Certificate, error) {
+	var res X509KeyData
+	var cert *x509.Certificate
+	var cr *cacheRecord
+	var err error
+	var ok bool
+
+	key_cache_requests.Add(1)
+
+	cl.cache_lock.RLock()
+	if cr, ok = cl.key_cache[index]; ok {
+		key_cache_hits.Add(1)
+		cr.LastUsed = time.Now()
+		defer cl.cache_lock.RUnlock()
+		return cr.Cert, nil
+	}
+	cl.cache_lock.RUnlock()
+
+	key_cache_misses.Add(1)
+	err = cl.client.Call(
+		"X509KeyServer.RetrieveCertificateByIndex",
+		X509KeyDataRequest{Index: proto.Uint64(index)}, &res)
+	if err != nil {
+		key_cache_errors.Add(err.Error(), 1)
+		return nil, err
+	}
+
+	cert, err = x509.ParseCertificate(res.GetDerCertificate())
+	if err != nil {
+		key_cache_errors.Add(err.Error(), 1)
+		return nil, err
+	}
+
+	cr = new(cacheRecord)
+	cr.Cert = cert
+	cr.LastUsed = time.Now()
+
+	cl.cache_lock.Lock()
+	cl.key_cache[index] = cr
+	key_cache_size.Set(int64(len(cl.key_cache)))
+	cl.cache_lock.Unlock()
+
+	return cert, nil
+}
---
+++ x509keycli/main.go
@@ -0,0 +1,91 @@
+/*
+ * (c) 2014, Tonnerre Lombard <tonnerre@ancient-solutions.com>,
+ *	     Starship Factory. All rights reserved.
+ *
+ * Redistribution and use in source  and binary forms, with or without
+ * modification, are permitted  provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of  source code  must retain the  above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this  list of conditions and the  following disclaimer in
+ *   the  documentation  and/or  other  materials  provided  with  the
+ *   distribution.
+ * * Neither  the name  of the Starship Factory  nor the  name  of its
+ *   contributors may  be used to endorse or  promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS"  AND ANY EXPRESS  OR IMPLIED WARRANTIES  OF MERCHANTABILITY
+ * AND FITNESS  FOR A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT
+ * SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED  TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE,  DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT  LIABILITY,  OR  TORT  (INCLUDING NEGLIGENCE  OR  OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package main
+
+import (
+	"expvar"
+	"flag"
+	"github.com/tonnerre/x509keyserver"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	var kc *x509keyserver.X509KeyClient
+	var fetch_interval, cache_prune_interval time.Duration
+	var server, fetch_ids, id string
+	var fetch_idlist []string
+	var max_records int
+	var err error
+
+	flag.StringVar(&server, "server", "localhost:8080",
+		"host:port pair of the x509 key server")
+	flag.IntVar(&max_records, "max-cache-records", 4,
+		"Maximum number of certificates to keep in the cache")
+	flag.StringVar(&fetch_ids, "ids", "",
+		"Comma-separated list of certificate IDs to fetch")
+	flag.DurationVar(&fetch_interval, "fetch-interval", 0,
+		"How long to wait between individual fetches (to test caching)")
+	flag.DurationVar(&cache_prune_interval, "cache-prune-interval", time.Second,
+		"How long to wait between two cache prunes (to test caching)")
+	flag.Parse()
+
+	kc, err = x509keyserver.NewX509KeyClient(server, max_records, time.Second)
+	if err != nil {
+		log.Fatal("Unable to connect to ", server, ": ", err)
+	}
+
+	fetch_idlist = strings.Split(fetch_ids, ",")
+	for _, id = range fetch_idlist {
+		var index uint64
+		var sz string
+		index, err = strconv.ParseUint(id, 10, 64)
+		if err != nil {
+			log.Print("Unable to parse ", id, " as a number, skipping.")
+			continue
+		}
+		sz = expvar.Get("x509-key-cache-size").String()
+		_, err = kc.RetrieveCertificateByIndex(index)
+		if err != nil {
+			log.Print("Error retrieving certificate ", index, ": ", err)
+		}
+		log.Print("Cache size: ", sz, " -> ", expvar.Get("x509-key-cache-size").String())
+		log.Print("Cache stats: hits: ", expvar.Get("x509-key-cache-hits").String(),
+			", misses: ", expvar.Get("x509-key-cache-misses"))
+
+		if fetch_interval > 0 {
+			time.Sleep(fetch_interval)
+		}
+	}
+}
